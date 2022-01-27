5c9cc13a0264ffb8a8ce04b2176bd1620e427921
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-27 18:09:05 -0300

    addTask function

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,25 @@ import { Task } from "./components/Task"
 
 function App() {
   const [tasks, setTasks] = useState<string[]>([])
+  const [description, setDescription] = useState<string>()
 
   const addTask = () => {
-    setTasks([...tasks, "new item"])
+    if(description) {
+      setTasks([...tasks, description])
+      setDescription('')
+    }
   }
 
   return (
     <div>
       {tasks.map((task, id) => {
-        return <Task key={id} text={`${task} ${id}`} />
+        return <Task key={id} text={task} />
       })}
-      <input name="description" type="text" />
+      <input
+        name="description"
+        type="text"
+        onChange={e => setDescription(e.target.value)}
+        value={description} />
       <button onClick={addTask}>Adicinar Tarefa</button>
     </div>
   )
