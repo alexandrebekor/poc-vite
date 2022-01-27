15388001ba9a74f43b130b4af10b48cfa71bb493
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-27 19:34:39 -0300

    added react-router-dom and /pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { useState } from "react"
-import { Task } from "./components/Task"
+import { Routes, Route } from "react-router-dom"
 import "./App.css"
+import { Card } from "./pages/Card"
+import { Home } from "./pages/Home"
 
 function App() {
-  const [tasks, setTasks] = useState<string[]>([])
-  const [description, setDescription] = useState<string>()
-
-  const addTask = () => {
-    if(description) {
-      setTasks([...tasks, description])
-      setDescription('')
-    }
-  }
-
   return (
     <div>
-      <input
-        name="description"
-        type="text"
-        onChange={e => setDescription(e.target.value)}
-        value={description} />
-      <button onClick={addTask}>Adicinar Tarefa</button>
-      {tasks.map((task, id) => {
-        return <Task key={id} text={task} />
-      })}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="card" element={<Card />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { Task } from "../components/Task"
+
+export function Home() {
+  const [tasks, setTasks] = useState<string[]>([])
+  const [description, setDescription] = useState<string>()
+
+  const addTask = () => {
+    if(description) {
+      setTasks([...tasks, description])
+      setDescription('')
+    }
+  }
+
+  return (
+    <div>
+      <input
+        name="description"
+        type="text"
+        onChange={e => setDescription(e.target.value)}
+        value={description} />
+      <button onClick={addTask}>Adicinar Tarefa</button>
+      {tasks.map((task, id) => {
+        return <Task key={id} text={task} />
+      })}
+    </div>
+  )
+}
