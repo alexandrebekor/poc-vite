4a9c2e88201369ba0c3c8eda1fb8749ed7d2cdde
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-28 10:08:17 -0300

    added style in task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -5,10 +5,11 @@ const Layout = css({
   display: 'flex',
   color: '#f1f1f1',
   backgroundColor: '#e1e',
-  padding: '0.5rem',
-  '&:checked': {
-    color: '#e1e'
-  }
+  padding: '0.5rem'
+})
+
+const Label = css({
+  textDecoration: 'line-through'
 })
 
 type TaskProps = {
@@ -16,10 +17,16 @@ type TaskProps = {
 }
 
 export function Task(props: TaskProps) {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = () => {
+    setChecked(!checked)
+  }
+
   return (
       <div className={Layout()}>
-        <input type="checkbox" />
-        <label>{ props.text }</label>
+        <input type="checkbox" checked={checked} onChange={handleChange} />
+        <label className={ checked ? Label() : '' }>{ props.text }</label>
       </div>
   )
 }
